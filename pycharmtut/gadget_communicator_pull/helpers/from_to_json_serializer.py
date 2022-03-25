89ea267437972e2e2b0e25049673a25de5fb0f54
i336317 <georgi.pavlov01@sap.com>
2022-03-25 02:05:40 +0200

    add is running to serializer 2

--- pycharmtut/gadget_communicator_pull/helpers/from_to_json_serializer.py
+++ pycharmtut/gadget_communicator_pull/helpers/from_to_json_serializer.py
@@ -1,6 +1,6 @@
 import json as simplejson
 
-from gadget_communicator_pull.water_serializers.constants.water_constants import DEVICES, HAS_BEEN_EXECUTED
+from gadget_communicator_pull.water_serializers.constants.water_constants import DEVICES, HAS_BEEN_EXECUTED, IS_RUNNING
 
 
 def to_json_serializer(serializer):
@@ -14,6 +14,11 @@ def remove_device_field_from_json(json_obj):
 
 def remove_has_been_executed_field(json_obj):
     json_obj.pop(HAS_BEEN_EXECUTED, None)
+    return json_obj
+
+
+def remove_is_running_field(json_obj):
+    json_obj.pop(IS_RUNNING, None)
     return json_obj
 
 
