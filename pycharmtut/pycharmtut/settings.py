1941c4fade1e65e5925f1fcc4d9845c89030c188
i336317 <georgi.pavlov01@sap.com>
2022-10-13 00:32:44 +0300

    request change port of vue

--- pycharmtut/pycharmtut/settings.py
+++ pycharmtut/pycharmtut/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aaaaaaaaaaaaaaaaaaaaaaaaaa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.20", "127.0.0.1", '192.168.1.31', '192.168.1.17', '192.168.1.9']
+ALLOWED_HOSTS = ["18.184.36.52", "192.168.1.20", "127.0.0.1", '192.168.1.31', '192.168.1.17','172.31.44.13','ip-172-31-44-13.eu-central-1.compute.internal', 'water-me-lb-842691727.eu-central-1.elb.amazonaws.com']
 
 # Application definition
 
@@ -76,13 +76,12 @@ WSGI_APPLICATION = 'pycharmtut.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 DATABASES = {
     'default': {
@@ -94,6 +93,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -189,6 +189,9 @@ JWT_AUTH = {
     'JWT_AUTH_COOKIE': None,
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CSRF_TRUSTED_ORIGINS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'http://192.168.1.2:8081',
     'http://192.168.0.209:8081',
@@ -201,8 +204,15 @@ CORS_ALLOWED_ORIGINS = [
     'http://192.168.1.8:8081',
     'http://192.168.1.12:8081',
     'http://192.168.1.17:8081',
-    'http://localhost:4173',
-    'http://127.0.0.1:4173',
-    'http://192.168.1.9:4173',
-    'http://192.168.1.9:8081',
+    'http://ip-172-31-44-13.eu-central-1.compute.internal:8080',
+    'http://ip-172-31-44-13.eu-central-1.compute.internal',
+    'http://172.31.44.13:8080',
+    'http://18.184.36.52:8080',
+    'http://172.31.44.13',
+    'http://18.184.36.52',
+    'http://18.184.36.52:80',
+    'http://water-me-lb-842691727.eu-central-1.elb.amazonaws.com:8080',
+    'http://water-me-lb-842691727.eu-central-1.elb.amazonaws.com:8080',
+    'http://water-me-lb-842691727.eu-central-1.elb.amazonaws.com',
+    'http://water-me-lb-842691727.eu-central-1.elb.amazonaws.com',
 ]
